27ef71af7ab471989ea4c05a512962d7f80e3a2c
rizkiromadoni <coradex06@gmail.com>
2023-08-13 04:16:48 +0700

    creating reusable modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useEffect } from "react";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal(state => state.onOpen);
+  const isOpen = useStoreModal(state => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
-        <UserButton afterSignOutUrl="/"/>
-        <p className="mt-5 pt-2 border-t-2">Admin Page</p>
+        Root Page
     </div>
   )
 }
