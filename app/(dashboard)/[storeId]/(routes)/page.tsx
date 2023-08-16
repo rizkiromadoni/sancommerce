c978527e70a465d73891bf385a477ee7d3df2635
rizkiromadoni <coradex06@gmail.com>
2023-08-16 08:53:30 +0700

    store selection in navbar

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,19 @@
-const DashboardPage = () => {
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+    params: { storeId: string }
+}
+
+const DashboardPage: React.FC<DashboardPageProps> =async ({params}) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    });
+
     return (
         <div>
-            Dashboard
+            {store?.name}
         </div>
     )
 }
--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -17,7 +18,7 @@ export default async function DashboardLayout({children, params}: {children: Rea
 
     return (
         <>
-            <div>Navbar</div>
+            <Navbar />
             {children}
         </>
     )
